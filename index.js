b9785187509d4617da69ddee83b3aa2b50c80030
Achi Onyinye <achionyinyegiftbarbie@gmail.com>
2022-01-20 08:22:35 +0100

    Our Pricing

---
+++ index.js
@@ -0,0 +1,35 @@
+
+function annualFunc(){
+    document.getElementById('priceBasic').innerHTML = ('199.99');
+    document.getElementById('priceProf').innerHTML = ('249.99');
+    document.getElementById('priceMaster').innerHTML = ('399.99');
+}
+
+function monthlyFunc(){
+    document.getElementById('priceBasic').innerHTML = ('19.99');
+    document.getElementById('priceProf').innerHTML = ('24.99');
+    document.getElementById('priceMaster').innerHTML = ('39.99');
+}
+
+
+
+
+// let priceBasic = document.querySelector("#priceBasic").value;
+// let priceProf = document.getElementById("#priceProf").value;
+// let priceMaster = document.getElementById("#priceMaster").value;
+// let toggle = document.querySelector('.switch');
+// console.log(priceBasic, priceProf, priceMaster)
+
+// // function monthlyFunc(priceBasic, priceProf, priceMaster){
+// //      priceBasic = 19.99;
+// //      priceProf = 24.99;
+// //      priceMaster = 39.99;
+// // }
+
+//  toggle.addEventListener('click', annualFunc)
+
+// function annualFunc(priceBasic, priceProf, priceMaster){
+//     priceBasic = 0.00;
+//     priceProf = 249.99;
+//     priceMaster = 399.99;
+// }
